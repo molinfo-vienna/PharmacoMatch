17343b668e27295b5d5f1d9c84c0d8ee90f798a3
DanielRose95 <danielrose95@gmx.de>
2024-03-26 09:44:22 +0100

    first working example of PhectorMatch

--- phector_db/model/projector.py
+++ phector_db/model/projector.py
@@ -24,3 +24,54 @@ class Projection(torch.nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         x = self.projection_head(x)
         return F.normalize(x, p=2, dim=1)
+
+
+class PositiveLinear(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        super(PositiveLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.log_weight = torch.nn.Parameter(torch.Tensor(out_features, in_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.log_weight)
+
+    def forward(self, input):
+        return torch.nn.functional.linear(input, self.log_weight.exp())
+
+
+class ProjectionPhectorMatch(torch.nn.Module):
+    def __init__(
+        self, input_dim: int = 2048, hidden_dim: int = 2048, output_dim: int = 128
+    ) -> None:
+        super().__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        # Projection head MLP
+        self.projection_head = torch.nn.Sequential(
+            Linear(self.input_dim, self.hidden_dim, bias=True),
+            torch.nn.BatchNorm1d(self.hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.3),
+            # Linear(self.hidden_dim, self.hidden_dim, bias=True),
+            # torch.nn.BatchNorm1d(self.hidden_dim),
+            # torch.nn.ReLU(),
+            # torch.nn.Dropout(p=0.3),
+            # Linear(self.hidden_dim, self.hidden_dim, bias=True),
+            # torch.nn.BatchNorm1d(self.hidden_dim),
+            # torch.nn.ReLU(),
+            # torch.nn.Dropout(p=0.3),
+            PositiveLinear(self.hidden_dim, self.output_dim),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.projection_head(x)
+        # x = torch.abs(x)
+        normalization = torch.norm(x, p=2, dim=1)
+        normalization = 1 / torch.max(torch.ones_like(normalization), normalization)
+        normalization = normalization.view(-1, 1).expand(-1, x.shape[1])
+        return x * normalization
+        # return F.normalize(x, p=2, dim=1)
--- phector_db/virtual_screening/virtual_screener.py
+++ phector_db/virtual_screening/virtual_screener.py
@@ -1,5 +1,6 @@
 import torch
 from torch import Tensor
+from torch_geometric.nn import global_max_pool
 
 from .feature_count_prefilter import FeatureCountPrefilter
 from .virtual_screening_embedder import VirtualScreeningEmbedder
@@ -24,46 +25,72 @@ class VirtualScreener:
         self.actives_prefilter_mask = self.prefilter.get_actives_mask(query_idx)
         self.inactives_prefilter_mask = self.prefilter.get_inactives_mask(query_idx)
 
-        # calculate cosine similarity of (in)actives w.r.t. to query
-        self.active_query_similarity = torch.mm(
-            self.query_embedding[query_idx].reshape(1, -1), self.active_embeddings.T
-        ).flatten()
-        self.inactive_query_similarity = torch.mm(
-            self.query_embedding[query_idx].reshape(1, -1), self.inactive_embeddings.T
-        ).flatten()
+        # calculate decision function of (in)actives w.r.t. to query
 
-        # pick most similar conformation per compound
-        self.active_mask = self._create_mask(
-            self.active_query_similarity, self.active_mol_ids
-        )
-        self.inactive_mask = self._create_mask(
-            self.inactive_query_similarity, self.inactive_mol_ids
-        )
-        self.top_active_similarity = self.active_query_similarity[self.active_mask]
-        self.top_inactive_similarity = self.inactive_query_similarity[
-            self.inactive_mask
-        ]
-        self.top_active_embeddings = self.active_embeddings[self.active_mask]
-        self.top_inactive_embeddings = self.inactive_embeddings[self.inactive_mask]
-
-        # Map similarity [-1, 1] --> [0, 1] and print AUC statistics
-        self.y_pred = torch.cat(
-            (self.top_active_similarity, self.top_inactive_similarity)
-        )
-        self.y_pred = (self.y_pred + 1) / 2
-        self.y_true = torch.cat(
-            (
-                torch.ones(len(self.top_active_similarity), dtype=torch.int),
-                torch.zeros(len(self.top_inactive_similarity), dtype=torch.int),
+        self.active_query_match = torch.sum(
+            torch.max(
+                torch.zeros_like(self.active_embeddings),
+                self.query_embedding.expand(self.active_embeddings.shape)
+                - self.active_embeddings,
             )
+            ** 2,
+            dim=1,
         )
+        self.inactive_query_match = torch.sum(
+            torch.max(
+                torch.zeros_like(self.inactive_embeddings),
+                self.query_embedding.expand(self.inactive_embeddings.shape)
+                - self.inactive_embeddings,
+            )
+            ** 2,
+            dim=1,
+        )
+        # self.active_query_match = torch.max(
+        #     self.query_embedding.expand(self.active_embeddings.shape)
+        #     - self.active_embeddings,
+        #     dim=1,
+        # ).values
+        # self.inactive_query_match = torch.max(
+        #     self.query_embedding.expand(self.inactive_embeddings.shape)
+        #     - self.inactive_embeddings,
+        #     dim=1,
+        # ).values
 
-    def _create_mask(self, similarities: Tensor, mol_ids: Tensor) -> Tensor:
-        splits = [similarities[mol_ids == i] for i in range(max(mol_ids + 1))]
-        split_argmax = [torch.argmax(split) for split in splits]
-        mask = [torch.zeros(split.shape) for split in splits]
-        for idx, mask_split in zip(split_argmax, mask):
-            mask_split[idx] = 1
-        mask = torch.cat(mask) != 0
+    #     self.active_match = global_max_pool(self.active_query_match, self.active_mol_ids)
+    #     self.inactive_match = global_max_pool(self.inactive_query_match, self.inactive_mol_ids)
 
-        return mask
+    #     # pick most similar conformation per compound
+    #     self.active_mask = self._create_mask(
+    #         self.active_query_match, self.active_mol_ids
+    #     )
+    #     self.inactive_mask = self._create_mask(
+    #         self.inactive_query_match, self.inactive_mol_ids
+    #     )
+    #     self.top_active_similarity = self.active_query_match[self.active_mask]
+    #     self.top_inactive_similarity = self.inactive_query_match[
+    #         self.inactive_mask
+    #     ]
+    #     self.top_active_embeddings = self.active_embeddings[self.active_mask]
+    #     self.top_inactive_embeddings = self.inactive_embeddings[self.inactive_mask]
+
+    #     # Map similarity [-1, 1] --> [0, 1] and print AUC statistics
+    #     self.y_pred = torch.cat(
+    #         (self.top_active_similarity, self.top_inactive_similarity)
+    #     )
+    #     self.y_pred = (self.y_pred + 1) / 2
+    #     self.y_true = torch.cat(
+    #         (
+    #             torch.ones(len(self.top_active_similarity), dtype=torch.int),
+    #             torch.zeros(len(self.top_inactive_similarity), dtype=torch.int),
+    #         )
+    #     )
+
+    # def _create_mask(self, similarities: Tensor, mol_ids: Tensor) -> Tensor:
+    #     splits = [similarities[mol_ids == i] for i in range(max(mol_ids + 1))]
+    #     split_argmax = [torch.argmax(split) for split in splits]
+    #     mask = [torch.zeros(split.shape) for split in splits]
+    #     for idx, mask_split in zip(split_argmax, mask):
+    #         mask_split[idx] = 1
+    #     mask = torch.cat(mask) != 0
+
+    #     return mask
