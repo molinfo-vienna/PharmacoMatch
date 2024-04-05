dd939f7b0e836398d02107298ba719d6d7e45d22
DanielRose95 <danielrose95@gmx.de>
2024-04-05 21:58:13 +0200

    first satisfactory result

--- phector_db/scripts/self_similarity.py
+++ phector_db/scripts/self_similarity.py
@@ -25,7 +25,7 @@ class SelfSimilarityEvaluation:
         steps_node_masking = 10
         self.max_radius = 10
         steps_radius = 6
-        self.max_threshold = 0.2
+        self.max_threshold = 5
         steps_threshold = 50
 
         self.node_masking_range = [
@@ -176,7 +176,7 @@ def run(device):
     PRETRAINING_ROOT = f"{PROJECT_ROOT}/training_data"
     VS_ROOT = f"{PROJECT_ROOT}/litpcba/ESR1_ant"
     MODEL = PhectorMatch
-    VERSION = 226
+    VERSION = 228
     MODEL_PATH = f"{PROJECT_ROOT}/logs/{MODEL.__name__}/version_{VERSION}/"
 
     params = yaml.load(
