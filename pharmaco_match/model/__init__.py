02a463a038406c8937a1ac79e0ee99a13510c205
DanielRose95 <danielrose95@gmx.de>
2024-08-14 19:07:07 +0200

    removed unnecessary code

--- pharmaco_match/model/__init__.py
+++ pharmaco_match/model/__init__.py
@@ -2,12 +2,10 @@ from .callbacks import (
     CurriculumLearningScheduler,
     ValidationDataTransformSetter,
 )
-from .pharm_clr import PharmCLR
 from .pharmaco_match import PharmacoMatch
 
 __all__ = [
     "CurriculumLearningScheduler",
     "ValidationDataTransformSetter",
-    "PharmCLR",
     "PharmacoMatch",
 ]
--- pharmaco_match/model/projector.py
+++ pharmaco_match/model/projector.py
@@ -88,32 +88,3 @@ class ProjectionPhectorMatch(torch.nn.Module):
             return x * normalization
         else:
             return x
-
-
-class Projection(torch.nn.Module):
-    def __init__(
-        self, input_dim: int = 2048, hidden_dim: int = 2048, output_dim: int = 128
-    ) -> None:
-        """A simple MLP that projects the output embeddings onto a unit sphere.
-
-        Args:
-            input_dim (int, optional): Input embedding dimension. Defaults to 2048.
-            hidden_dim (int, optional): Hidden embedding dimension. Defaults to 2048.
-            output_dim (int, optional): Output embedding dimension. Defaults to 128.
-        """
-        super().__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
-
-        # Projection head MLP
-        self.projection_head = torch.nn.Sequential(
-            Linear(self.input_dim, self.hidden_dim, bias=True),
-            torch.nn.BatchNorm1d(self.hidden_dim),
-            torch.nn.GELU(),
-            torch.nn.Linear(self.hidden_dim, self.output_dim),
-        )
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.projection_head(x)
-        return F.normalize(x, p=2, dim=1)
